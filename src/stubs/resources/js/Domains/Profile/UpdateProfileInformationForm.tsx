90b2fdb189b279fc3a444e77bdef757c9ce9dc39
Ozzie Neher <oz.neher@gmail.com>
2021-11-04 08:54:06 -0600

    Update UpdateProfileInformationForm.tsx

--- src/stubs/resources/js/Domains/Profile/UpdateProfileInformationForm.tsx
+++ src/stubs/resources/js/Domains/Profile/UpdateProfileInformationForm.tsx
@@ -29,13 +29,6 @@ export default function UpdateProfileInformationForm({ user }: Props) {
   const page = usePage<any>();
 
   function updateProfileInformation() {
-    if (photoRef.current) {
-      form.transform(data => ({
-        ...data,
-        photo: photoRef.current?.files?.[0] || null,
-      }));
-    }
-
     form.post(route('user-profile-information.update'), {
       errorBag: 'updateProfileInformation',
       preserveScroll: true,
@@ -50,7 +43,11 @@ export default function UpdateProfileInformationForm({ user }: Props) {
   function updatePhotoPreview() {
     const photo = photoRef.current?.files?.[0];
 
-    if (!photo) return;
+    if (!photo) {
+      return;
+    }
+
+    form.setData('photo', photo);
 
     const reader = new FileReader();
 

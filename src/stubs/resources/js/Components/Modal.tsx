88c68967bdba744b153c1a791c4ee6d361f9e62c
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-12-06 08:12:43 +0700

    wip

--- src/stubs/resources/js/Components/Modal.tsx
+++ src/stubs/resources/js/Components/Modal.tsx
@@ -65,14 +65,14 @@ export default function Modal({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div
+            <Dialog.Panel
               className={classNames(
                 'inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full',
                 maxWidthClass,
               )}
             >
               {children}
-            </div>
+            </Dialog.Panel>
           </Transition.Child>
         </div>
       </Dialog>
